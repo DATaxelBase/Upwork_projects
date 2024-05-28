4656daaedf06f7f888a891fe282e24a105cda526
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-05-28 04:43:58 +0200

    Update max_pain_scraping.py

--- max_pain_scraping.py
+++ max_pain_scraping.py
@@ -9,6 +9,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common import exceptions
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
 # define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
@@ -21,7 +54,6 @@ sheet = client.open('Test_deployment')#Name of sheet
 sheet_instance = sheet.get_worksheet(0)
 records_data = sheet_instance.col_values(1)
 
-driver = webdriver.Firefox()
 issue_on = []
 for el in range(2,len(records_data)):
     driver.get('https://maximum-pain.com/options/'+records_data[el]) #Access to element
