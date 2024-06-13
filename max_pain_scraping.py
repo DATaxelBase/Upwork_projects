7ff338c9413e648e43281d7f72b5e07bf74149a0
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-06-14 00:29:11 +0200

    Update max_pain_scraping.py

--- max_pain_scraping.py
+++ max_pain_scraping.py
@@ -28,17 +28,17 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 
 # add credentials to the account
 data = {
-  "type": str(os.environ["type"]),
-  "project_id": str(os.environ["project_id"]),
-  "private_key_id": str(os.environ["private_key_id"]),
-  "private_key": str(os.environ["private_key"]),
-  "client_email": str(os.environ["client_email"]),
-  "client_id": str(os.environ["client_id"]),
-  "auth_uri": str(os.environ["auth_uri"]),
-  "token_uri": str(os.environ["token_uri"]),
-  "auth_provider_x509_cert_url": str(os.environ["auth_provider_x509_cert_url"]),
-  "client_x509_cert_url": str(os.environ["client_x509_cert_url"]),
-  "universe_domain": str(os.environ["universe_domain"])
+  "type": os.environ["type"],
+  "project_id": os.environ["project_id"],
+  "private_key_id": os.environ["private_key_id"],
+  "private_key": os.environ["private_key"],
+  "client_email": os.environ["client_email"],
+  "client_id": os.environ["client_id"],
+  "auth_uri": os.environ["auth_uri"],
+  "token_uri": os.environ["token_uri"],
+  "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": os.environ["client_x509_cert_url"],
+  "universe_domain": os.environ["universe_domain"]
 }
 creds = ServiceAccountCredentials.from_json_keyfile_dict(data, scope)
 # authorize the clientsheet 
