8317c70aba61049b5dc9a9da3137d6f616cf286c
DATaxelBase <112965837+DATaxelBase@users.noreply.github.com>
2024-06-14 00:56:14 +0200

    Update max_pain_scraping.py

--- max_pain_scraping.py
+++ max_pain_scraping.py
@@ -27,7 +27,19 @@ driver = webdriver.Chrome()
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
 # add credentials to the account
-data = os.environ["data"]
+data = {
+  "type": os.environ["type"],
+  "project_id": os.environ["project_id"],
+  "private_key_id": os.environ["private_key_id"],
+  "private_key": os.environ["private_key"],
+  "client_email": os.environ["client_email"],
+  "client_id": os.environ["client_id"],
+  "auth_uri": os.environ["auth_uri"],
+  "token_uri": os.environ["token_uri"],
+  "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": os.environ["client_x509_cert_url"],
+  "universe_domain": os.environ["universe_domain"]
+}
 creds = ServiceAccountCredentials.from_json_keyfile_dict(data, scope)
 # authorize the clientsheet 
 client = gspread.authorize(creds)
